69459a032357c6410339c1d5b36806ba11a6dce6
Pierre-Bi <59541123+Pierre-Bi@users.noreply.github.com>
2020-01-05 21:37:27 +0100

    Delete easteregg.js

--- easteregg.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("spanpoeme").style.visibility = "hidden";
-
-function togglePoeme() {
-	if(document.getElementById("spanpoeme").style.visibility === "hidden"){
-		document.getElementById("spanpoeme").style.visibility = "visible";
-	} else {
-		document.getElementById("spanpoeme").style.visibility = "hidden";
-	}
-}
